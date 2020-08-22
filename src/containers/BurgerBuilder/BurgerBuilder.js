f851294e2fe5814d93361d6ef3e07784b375cb38
Sean Charoutanh <sean@Seans-MacBook-Pro.local>
2020-08-21 22:06:00 -0600

    Send Post Request To FireBase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -79,7 +80,30 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert("You Continued!");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Sean',
+                address: {
+                    street: 'Test St W.',
+                    zipCode: '89867',
+                    country: 'United States'
+                },
+                email: 'test@test.com',
+                delivery: 'fastest'
+            }
+        }
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+        //alert("You Continued!");
     }
 
     render() {
